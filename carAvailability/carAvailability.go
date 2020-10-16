09a73777aa6ca6e70b1e0b3619e8bf8a515990ba
Richard Peres <richardperes.info@gmail.com>
2020-10-16 12:01:17 +0200

    Push car Availability logic (TODO mock)

--- carAvailability/carAvailability.go
+++ carAvailability/carAvailability.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/robpike/filter"
 )
+
+// Custom error to return in case of a JSON parsing error
+type JSONError struct {
+	Message string `json:"Message"`
+}
+
+// A Car representation for this svc
+type Car struct {
+	Id        int       `json:"id"`
+	WagonType string    `json:"wagonType"` // TODO replace with enum ?
+	Date      time.Time `json:"date"`
+}
 
 // Basic OK route for healthcheck
 func ok(w http.ResponseWriter, req *http.Request) {
@@ -15,6 +31,73 @@ func ok(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// TODO Should return the list of car booked from DB and/or CarBooking service
+func carsBookedList() []Car {
+	carsBooked := make([]Car, 0)
+
+	// TODO logic
+
+	return carsBooked
+}
+
+// Filters & returns the list of all booked cars by filters
+func getNonAvailableCars(date time.Time, wagonType string) []Car {
+	var carsBookedFiltered []Car
+	carsBooked := carsBookedList()
+
+	// TODO logic
+	var i interface{} = filter.Choose(carsBooked, func(car Car) bool {
+		return car.WagonType == wagonType && car.Date.YearDay() == date.YearDay()
+	})
+	carsBookedFiltered, ok := i.([]Car)
+
+	if !ok {
+		log.Println("Error filtering booked cars")
+	}
+
+	return carsBookedFiltered
+}
+
+// Return the list of all car unavailable with given filters
+func getNonAvailableCarsRoute(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := req.URL.Query()
+
+	// Get the date from parameter
+	dateParam, ok := params["date"]
+	if !ok {
+		log.Println("Error getNonAvailableCarsRoute : Date parameter not provided")
+		return
+	}
+	// Convert DateParam into date
+	date, err := time.Parse(time.RFC3339, dateParam[0])
+	if err != nil {
+		log.Println("Error getNonAvailableCarsRoute : Date parameter incorrect")
+		log.Panic(err)
+		return
+	}
+
+	// Get the wagonType from parameter
+	wagonTypeParam, ok := params["wagonType"]
+	if !ok {
+		log.Fatalln("Error getNonAvailableCarsRoute : WagonType parameter not provided")
+		return
+	}
+	wagonType := wagonTypeParam[0]
+
+	cars := getNonAvailableCars(date, wagonType)
+
+	// Return logs as a JSON object
+	jsonError := json.NewEncoder(w).Encode(cars)
+	if jsonError != nil {
+		e := JSONError{Message: "Internal Server Error"}
+		w.WriteHeader(http.StatusInternalServerError)
+		err := json.NewEncoder(w).Encode(e)
+		log.Panic(err)
+	}
+
 }
 
 func main() {
@@ -30,6 +113,7 @@ func main() {
 
 	// All the routes of the app
 	router.HandleFunc("/car-availability/ok", ok).Methods("GET")
+	router.HandleFunc("/car-availability/getNonAvailableCars", getNonAvailableCarsRoute).Methods("GET")
 
 	fmt.Println("Server is running on port " + port)
 
--- carAvailability/go.mod
+++ carAvailability/go.mod
@@ -2,4 +2,7 @@ module carAvailability
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/robpike/filter v0.0.0-20150108201509-2984852a2183
+)
