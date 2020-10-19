494fc2ba189fe8fde6174ce19ded292be9dde579
SylvainMasia <sylvain.masia@etu.unice.fr>
2020-10-19 22:27:08 +0200

    #1 Supplier entity creation

---
+++ bookingProcess/entities/offer.go
@@ -0,0 +1,13 @@
+package entities
+
+import "time"
+
+type Offer struct {
+	ID        int      `json:"id"`
+	BookDate time.Time `json:"bookDate"`
+	Arrival 		Node		`json:"arrivalNode"`
+	Departure 		Node		`json:"departureNode"`
+	Car 			Car			`json:"car"`
+	Price float32 `json:"price"`
+
+}
