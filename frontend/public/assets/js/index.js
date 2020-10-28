8ba8f5a0f50e77270da29a7c530aad75c71c2701
Damien Montoya <damien.montoya26@gmail.com>
2020-10-28 19:37:25 +0100

    front end done + cors on offer

--- frontend/public/assets/js/index.js
+++ frontend/public/assets/js/index.js
@@ -27,34 +27,55 @@ class Node{
 
 listNodes = []
 
+let carTypeIdSelected = -1;
+let nodeArrivalIdSelected = -1;
+let nodeDepartureIdSelected = -1;
+let supplierInput = null;
 let carTypeIdSelect = null;
 let nodeArrivalSelect = null;
 let nodeDepartureSelect = null;
+let dateTimeDeparture = null;
+let loadingBigContainer = null;
 
-function addSelectNodes(select, carTypeId){
+function addNodesToSelect(select, firstInit = false){
     select.innerHTML = "";
     if(select === nodeArrivalSelect){
-        addOptionToSelect(select, "Aucun noeud d'arrivée sélectionné", 0, true)
+        addOptionToSelect(select, "Aucun noeud d'arrivée sélectionné", 0, true, null)
     }else{
-        addOptionToSelect(select, "Aucun noeud de départ sélectionné", 0, true)
+        addOptionToSelect(select, "Aucun noeud de départ sélectionné", 0, true, null)
     }
     select.disabled = false;
 
-    listNodes.forEach(function (node){
-        if(carTypeId === 0 || node.hasCarType(carTypeId)){
-           addOptionToSelect(select, node.name, node.id);
+    for (let i = 0; i < listNodes.length; i++){
+        const node = listNodes[i];
+        if(node.hasCarType(carTypeIdSelected)){
+            if(i === 0 && firstInit){
+                if(select === nodeArrivalSelect){
+                    nodeArrivalIdSelected = node.id;
+                }else{
+                    nodeDepartureIdSelected = node.id;
+                }
+            }
+            if(select === nodeDepartureSelect && node.id !== nodeArrivalIdSelected){
+                addOptionToSelect(select, node.name, node.id, false, nodeDepartureIdSelected);
+            }
+
+            if(select === nodeArrivalSelect && node.id !== nodeDepartureIdSelected) {
+                addOptionToSelect(select, node.name, node.id, false, nodeArrivalIdSelected);
+            }
         }
-    });
+    }
 }
 
-function addOptionToSelect(select, name, value, disabled){
+function addOptionToSelect(select, name, value, disabled, selectedValue){
     let option = document.createElement("option");
     option.value = value;
     option.disabled = disabled
+    if (value === selectedValue)
+        option.selected = true
     option.appendChild(document.createTextNode(name))
     select.appendChild(option);
 }
-
 
 function loadNodes(){
     let loadIdMember = new XMLHttpRequest();
@@ -64,8 +85,8 @@ function loadNodes(){
             let response = JSON.parse(this.responseText);
             response.forEach(function(node){
                listNodes.push(new Node(node));
-                addOptionToSelect(nodeDepartureSelect, node.name, node.id, false);
-                addOptionToSelect(nodeArrivalSelect, node.name, node.id, false);
+                addOptionToSelect(nodeDepartureSelect, node.name, node.id, false, null);
+                addOptionToSelect(nodeArrivalSelect, node.name, node.id, false, null);
             });
         }
     });
@@ -80,7 +101,7 @@ function loadCarTypes(){
             let response = JSON.parse(this.responseText);
             response.forEach(function(carType){
                 let carTypeObject = new CarType(carType)
-                addOptionToSelect(carTypeIdSelect, carTypeObject.name, carTypeObject.id);
+                addOptionToSelect(carTypeIdSelect, carTypeObject.name, carTypeObject.id, false, null);
             });
         }
     });
@@ -88,17 +109,94 @@ function loadCarTypes(){
 }
 
 (function(){
+    loadingBigContainer = document.getElementById("loading-big-container");
+    nodeDepartureSelect = document.getElementById("node-departure-select");
+    nodeArrivalSelect = document.getElementById("node-arrival-select");
     carTypeIdSelect = document.getElementById("car-type-select");
+    supplierInput = document.getElementById("supplier");
 
     let buttonSubmit = document.getElementById("middle-form-submit");
 
     carTypeIdSelect.addEventListener("change", function(e){ // when selecting an other value
+        carTypeIdSelected = parseInt(e.target.value);
         buttonSubmit.disabled = false;
-        addSelectNodes(nodeDepartureSelect, parseInt(e.target.value))
-        addSelectNodes(nodeArrivalSelect, parseInt(e.target.value))
+        addNodesToSelect(nodeDepartureSelect, true)
+        addNodesToSelect(nodeArrivalSelect, true)
     });
-    nodeDepartureSelect = document.getElementById("node-departure-select");
-    nodeArrivalSelect = document.getElementById("node-arrival-select");
+    nodeDepartureSelect.addEventListener("change", function(e){
+        nodeDepartureIdSelected = parseInt(e.target.value);
+        addNodesToSelect(nodeArrivalSelect);
+        addNodesToSelect(nodeDepartureSelect);
+    });
+    nodeArrivalSelect.addEventListener("change", function(e){
+        nodeArrivalIdSelected = parseInt(e.target.value);
+        addNodesToSelect(nodeDepartureSelect);
+        addNodesToSelect(nodeArrivalSelect);
+    });
+    document.getElementById("date-departure").addEventListener("input", function(e){
+        dateTimeDeparture = e.target.value;
+    });
+    document.getElementById("middle-form").addEventListener("submit", handleFormSubmit);
     loadNodes();
     loadCarTypes();
 })()
+
+function handleFormSubmit(e){
+    e.preventDefault();
+    if (carTypeIdSelected === -1){
+        alert("Vous devez choisir un type de wagon");
+        return;
+    }
+    if (nodeDepartureIdSelected === -1){
+        alert("Vous devez choisir un noeud de départ");
+        return;
+    }
+    if (nodeArrivalIdSelected === -1){
+        alert("Vous devez choisir un noeud d'arrivé");
+        return;
+    }
+    if (dateTimeDeparture === null){
+        alert("Vous devez choisir une date et une heure de départ");
+        return;
+    }
+    if(supplierInput.value === null || supplierInput.value.length === 0){
+        alert("Le nom du fournisseur doit être renseigné");
+        return;
+    }
+    launchSearch();
+}
+
+function addLoader(){
+    let loadingContainer = document.getElementById("loading-container");
+    if(loadingContainer !== null)
+        return;
+    loadingContainer = document.createElement('div');
+    loadingContainer.id = 'loading-container';
+    const loader = document.createElement('div');
+    loader.classList.add('loader');
+    loadingContainer.appendChild(loader);
+    loadingBigContainer.appendChild(loadingContainer);
+}
+
+function removeLoader(){
+    loadingBigContainer.innerHTML = "";
+}
+
+function launchSearch(){
+    addLoader();
+    let loadIdMember = new XMLHttpRequest();
+    loadIdMember.open('POST', 'http://localhost/booking-process/offers', true);
+    loadIdMember.addEventListener('readystatechange', function() {
+        if(this.readyState === 4 && this.status === 200) {
+            removeLoader();
+            let response = JSON.parse(this.responseText);
+            console.log(response)
+        }
+    });
+    loadIdMember.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    loadIdMember.send('departureNodeId=' + nodeDepartureIdSelected +
+        "&arrivalNodeId=" + nodeArrivalIdSelected +
+        "&dateTimeDeparture=" + dateTimeDeparture +
+        "&supplier=" + supplierInput.value +
+        "&carTypeId=" + carTypeIdSelected);
+}
