1ee5ce9758e870d2122baf6256ddf7d93fda5af4
FrancoisRigaut <francois.rigaut@etu.unice.fr>
2020-11-01 18:13:23 +0100

    Improved nodes mocking (added distances)

--- carLocation/src/repositories/neo4j_repository.js
+++ carLocation/src/repositories/neo4j_repository.js
@@ -9,9 +9,9 @@ function initNeo4jSession() {
     )
 }
 
-function addNode(id, name, types, lat, lgt) {
+async function addNode(id, name, types, lat, lgt) {
     var session = driver.session()
-    session.run('CREATE (a:Node {id: $id, name: $name, types: $types, latitude: $lat, longitude: $lgt}) RETURN a', {
+    await session.run('CREATE (a:Node {id: $id, name: $name, types: $types, latitude: $lat, longitude: $lgt}) RETURN a', {
         name: name,
         id: neo4j.int(id),
         types: types,
@@ -28,17 +28,42 @@ function addNode(id, name, types, lat, lgt) {
     })
 }
 
+async function addDistance(idNode1, idNode2, value) {
+    var session = driver.session()
+    await session.run('MATCH (a: Node), (b: Node) WHERE a.id = $idNode1 AND b.id = $idNode2 CREATE (a)-[d: Distance {value: $value}]->(b) RETURN a, b, d', {
+        idNode1: neo4j.int(idNode1),
+        idNode2: neo4j.int(idNode2),
+        value: neo4j.int(value)
+    }).subscribe({
+        onNext: record => {
+            console.log('Added relation: ' + JSON.stringify(record))
+        },
+
+        onCompleted: () => {
+            session.close() // returns a Promise
+        },
+    })
+}
+
 async function deleteAllNodes() {
     var session = driver.session()
-    await session.run('MATCH (a:Node) DELETE a', {})
+    await session.run('MATCH p=()-[r:Distance]->() DELETE p', {})
     await session.close()
 }
 
 async function populateDatabase() {
     await deleteAllNodes()
-    addNode(0, 'Marseille', ['liquid','solid'], 43.9415356, 4.7632126)
-    addNode(1, 'Avignon', ['liquid'], 43.9415356, 4.7632126)
-    addNode(2, 'Nice', ['solid'], 43.7031691, 7.1827772)
+    await addNode(0, 'Marseille', ['liquid','solid'], 43.9415356, 4.7632126)
+    await addNode(1, 'Avignon-liquid', ['liquid'], 43.9415356, 4.7632126)
+    await addNode(2, 'Nice', ['solid'], 43.7031691, 7.1827772)
+    await addNode(3, 'Paris', ['solid', 'liquid'], 48.8588377, 2.2770202)
+    await addNode(4, 'Avignon-solid', ['solid'], 43.9415387, 4.7632200)
+
+    await addDistance(0,1, 85)
+    await addDistance(0,2, 215)
+    await addDistance(1, 2, 199)
+    await addDistance(0, 3, 660)
+    await addDistance(1, 4, 20)
 }
 
 module.exports =  {
