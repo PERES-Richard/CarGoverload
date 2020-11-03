f27413f9ea6b6981ed34629dc34f680938e501fc
Damien Montoya <damien.montoya26@gmail.com>
2020-11-03 22:46:53 +0100

    fixing deleting node

--- carLocation/src/repositories/neo4j_repository.js
+++ carLocation/src/repositories/neo4j_repository.js
@@ -10,84 +10,51 @@ function initNeo4jSession() {
 }
 
 async function addCarType(id, name) {
-    const session = driver.session()
+    const session = driver.session();
     await session.run('CREATE (c:CarType {id: $id, name: $name}) RETURN c', {
         name: name,
         id: neo4j.int(id),
-    }).subscribe({
-        onNext: record => {
-            console.log('Added car type: ' + JSON.stringify(record))
-        },
-
-        onCompleted: () => {
-            session.close() // returns a Promise
-        },
-    })
+    });
+    await session.close();
+    console.log("CarType added : " + id + " - " + name);
 }
 
 async function addNode(id, name, types, lat, lgt) {
-    const session = driver.session()
+    const session = driver.session();
     await session.run('CREATE (a:Node {id: $id, name: $name, types: $types, latitude: $lat, longitude: $lgt}) RETURN a', {
         name: name,
         id: neo4j.int(id),
         types: types,
         lat: lat,
         lgt: lgt
-    }).subscribe({
-        onNext: record => {
-            console.log('Added node: ' + JSON.stringify(record))
-        },
-
-        onCompleted: () => {
-            session.close() // returns a Promise
-        },
-    })
+    });
+    await session.close();
+    console.log("Node added : " + id + " - " + name);
 }
 
 async function addDistance(idNode1, idNode2, value) {
-    const session = driver.session()
+    const session = driver.session();
     await session.run('MATCH (a: Node), (b: Node) WHERE a.id = $idNode1 AND b.id = $idNode2 CREATE (a)-[d: Distance {value: $value}]->(b) RETURN a, b, d', {
         idNode1: neo4j.int(idNode1),
         idNode2: neo4j.int(idNode2),
         value: neo4j.int(value)
-    }).subscribe({
-        onNext: record => {
-            console.log('Added relation: ' + JSON.stringify(record))
-        },
-
-        onCompleted: () => {
-            session.close() // returns a Promise
-        },
-    })
+    });
+    await session.close();
+    console.log("Distance added : " + idNode1 + " -> " + value + " -> " + idNode2);
 }
 
 async function getAllNodes(res) {
     const session = driver.session();
-    const nodes = [];
-    await session.run('MATCH (a: Node) RETURN DISTINCT a', {}).subscribe({
-        onNext: record => {
-            console.log('Fetched node: ' + JSON.stringify(record))
-            nodes.push(record);
-        },
-
-        onCompleted: () => {
-            res.send(JSON.stringify(nodes));
-            session.close() // returns a Promise
-        },
-    })
+    const records = await session.run('MATCH (a: Node) RETURN DISTINCT a', {})
+    await session.close();
+    res.send(JSON.stringify(records.records));
 }
 
 async function deleteAllNodes() {
-    const session = driver.session()
-    await session.run('MATCH p=()-[r:Distance]->() DELETE p', {}).subscribe({
-        onNext: () => {
-            console.log('Deleted all nodes')
-        },
-
-        onCompleted: () => {
-            session.close() // returns a Promise
-        },
-    })
+    const session = driver.session();
+    await session.run('MATCH p=()-[r:Distance]->() DELETE p', {});
+    await session.close();
+    console.log("Deleting all nodes");
 }
 
 async function getAllCarTypes(res) {
@@ -107,35 +74,32 @@ async function getAllCarTypes(res) {
 }
 
 async function deleteAllCarTypes() {
-    const session = driver.session()
-    await session.run('MATCH (c: CarType) DELETE c', {}).subscribe({
-        onNext: () => {
-            console.log('Deleted all nodes')
-        },
-
-        onCompleted: () => {
-            session.close() // returns a Promise
-        },
-    })
+    const session = driver.session();
+    await session.run('MATCH (c: CarType) DELETE c', {});
+    await session.close();
+    console.log("Deleting all car types");
 }
 
 async function populateDatabase() {
     await deleteAllCarTypes();
+    await deleteAllNodes();
+
+
     await addCarType(0, "Solid");
     await addCarType(1, "Liquid");
 
-    await deleteAllNodes()
-    await addNode(0, 'Marseille', [0, 1], 43.9415356, 4.7632126)
-    await addNode(1, 'Avignon-liquid', [1], 43.9415356, 4.7632126)
-    await addNode(2, 'Nice', [0], 43.7031691, 7.1827772)
-    await addNode(3, 'Paris', [0, 1], 48.8588377, 2.2770202)
-    await addNode(4, 'Avignon-solid', [0], 43.9415387, 4.7632200)
 
-    await addDistance(0,1, 85)
-    await addDistance(0,2, 215)
-    await addDistance(1, 2, 199)
-    await addDistance(0, 3, 660)
-    await addDistance(1, 4, 20)
+    await addNode(0, 'Marseille', [0, 1], 43.9415356, 4.7632126);
+    await addNode(1, 'Avignon-liquid', [1], 43.9415356, 4.7632126);
+    await addNode(2, 'Nice', [0], 43.7031691, 7.1827772);
+    await addNode(3, 'Paris', [0, 1], 48.8588377, 2.2770202);
+    await addNode(4, 'Avignon-solid', [0], 43.9415387, 4.7632200);
+
+    await addDistance(0,1, 85);
+    await addDistance(0,2, 215);
+    await addDistance(1, 2, 199);
+    await addDistance(0, 3, 660);
+    await addDistance(1, 4, 20);
 }
 
 module.exports =  {
