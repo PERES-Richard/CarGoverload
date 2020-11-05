a3f8f057c820af997f6ce21bda2a5ff94b4c954f
FrancoisRigaut <francois.rigaut@etu.unice.fr>
2020-11-05 20:47:51 +0100

    corrected return filtering in carTracking

--- carTracking/services/tracking-service.go
+++ carTracking/services/tracking-service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"carTracking/entities"
+	"strconv"
 )
 
 type TrackingService struct {
@@ -18,11 +19,19 @@ func (s *TrackingService) GetCars(latitude string, longitude string, carType str
 	// MOCKING - IRL we would have an algorithm to get cars close to the location specified
 	// by the nodeId parameter and corresponding to the carType parameter,
 	// but for now we return a set list of cars
-	return []entities.Car{
+	cars := []entities.Car{
 		{Id: 1, CarType: entities.CarType{Name: "Liquid", Id: 1}},
 		{Id: 2, CarType: entities.CarType{Name: "Solid", Id: 2}},
 		{Id: 3, CarType: entities.CarType{Name: "Solid", Id: 2}},
 		{Id: 4, CarType: entities.CarType{Name: "Liquid", Id: 1}},
 		{Id: 5, CarType: entities.CarType{Name: "Liquid", Id: 2}},
 	}
+	res := make([]entities.Car,0)
+	for _,c := range cars {
+		ct, _ := strconv.Atoi(carType)
+		if c.CarType.Id == ct {
+			res = append(res, c)
+		}
+	}
+	return res
 }
