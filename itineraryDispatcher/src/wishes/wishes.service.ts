cb6ec46747aa31268bef3e83b5fd43bf0df93edc
SylvainMasia <sylvain.masia@etu.unice.fr>
2021-01-29 10:39:02 +0100

    #16 Producing client type fix for ItineraryDispatcher

--- itineraryDispatcher/src/wishes/wishes.service.ts
+++ itineraryDispatcher/src/wishes/wishes.service.ts
@@ -1,7 +1,8 @@
 import {Inject, Injectable} from '@nestjs/common';
+import {ClientKafka} from "@nestjs/microservices";
 
 @Injectable()
 export class WishesService {
-    constructor(@Inject("DISPATCHER_SERVICE") private readonly kafkaClient) {}
+    constructor(@Inject("DISPATCHER_SERVICE") private readonly kafkaClient : ClientKafka) {}
 
 }
