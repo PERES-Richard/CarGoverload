a7012ff138c97731ea42044a389c1790259ec0dd
SylvainMasia <sylvain.masia@etu.unice.fr>
2021-02-16 22:09:56 +0100

    #5 ItineraryDispatcher take wish id from bookingProcess

--- itineraryDispatcher/src/wishes/wishes.service.ts
+++ itineraryDispatcher/src/wishes/wishes.service.ts
@@ -1,20 +1,21 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ClientKafka } from "@nestjs/microservices";
 import { SearchEvent } from 'src/models/search_event';
-import { WishDTO } from 'src/models/wish_dto';
+import { WishDTO, wishRequest } from 'src/models/wish_dto';
 import { WishEvent } from 'src/models/wish_event';
 
 @Injectable()
 export class WishesService {
     constructor(@Inject("DISPATCHER_SERVICE") private readonly kafkaClient: ClientKafka) { }
 
-    async dispatchWishes(wishes: WishDTO[]) {
-        const requestTimestamp = Date.now();
-        let wishEvent: WishEvent = { wishId: `${requestTimestamp}`, searchIds: [] };
+    async dispatchWishes(wishRequest : wishRequest) {
+        const WISH_ID = wishRequest.wishId;
+        let wishes = wishRequest.wishes;
+        let wishEvent: WishEvent = { wishId: `${WISH_ID}`, searchIds: [] };
         for (let indexOfWishes = 0; indexOfWishes < wishes.length; indexOfWishes++) {
             const wish = wishes[indexOfWishes];
             for (let indexOfCars = 0; indexOfCars < wish.numberOfCars; indexOfCars++) {
-                let searchEvent: SearchEvent = { searchId: `${requestTimestamp}_${indexOfWishes}-${wish.carType.charAt(0).toUpperCase()}${indexOfCars}`, carType: wish.carType, departureNode: wish.departureNode, arrivalNode: wish.arrivalNode, dateDeparture: wish.dateDeparture };
+                let searchEvent: SearchEvent = { searchId: `${WISH_ID}_${indexOfWishes}-${wish.carType.charAt(0).toUpperCase()}${indexOfCars}`, carType: wish.carType, departureNode: wish.departureNode, arrivalNode: wish.arrivalNode, dateDeparture: wish.dateDeparture };
                 wishEvent.searchIds.push(searchEvent.searchId);
                 await this.kafkaClient.emit(`new-search`, searchEvent).toPromise();
             }
